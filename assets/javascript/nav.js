1bbaaf73404bb6d07cdcdaff689d00c081522fd1
Eric Traband <ejstraband@gmail.com>
2018-01-23 19:29:34 -0600

    added joys nav.js file

--- assets/javascript/nav.js
+++ assets/javascript/nav.js
@@ -1,16 +1,93 @@
+// $(document).ready(function() {
+
+// // console.log('JOY!');
+
+// function hideAllDiv(){
+// $('#statzPage, #listPage, #last4Page, #calendarPage, #memebersPage, #last4TrailerPage, #landingPage').each(function() {
+// 	$(this).hide();
+// });
+// }
+
+// // Make only the landing page show up.
+// hideAllDiv();
+// $('#landingPage').show();
+// });
+
+
 $(document).ready(function() {
 
-// console.log('JOY!');
+console.log('joy!');
 
+});
+create a function to hide all divs
 function hideAllDiv(){
-$('#statzPage, #listPage, #last4Page, #calendarPage, #memebersPage, #last4TrailerPage, #landingPage').each(function() {
+$('#statzPage, #listPage, #last4Page, #calendarPage, #membersPage, #last4TrailerPage, #landingPage').each(function() {
 	$(this).hide();
-});
-}
+};
 
-// Make only the landing page show up.
-hideAllDiv();
-$('#landingPage').show();
+// // Make only the landing page show up.
+
+// // When a link is clicked, hide the currently visible page and display the clicked pg
+
+
+$("#statzPage-link").on("click", function() {
+   hideAllDiv();
+   $("#statzPage").show();
+   $("#navFooter").show();
+   console.log(hideAllDiv);
+
+$("#listPage-link").click(function() {
+    hideAllDiv();
+   $("#listPage").show();
 });
 
+$("#last4-link").click(function() {
+    hideAllDiv();
+   $("#last4Page").show();
+
+});
+
+$("#membersPage-link").click(function() {
+   hideAllDiv();
+   $("#membersPage").show();
+});
+
+$("#calendarPage-link").click(function() {
+    hideAllDiv();
+   $("#calendarPage").show();
+});
+
+$("#addMovie-link").click(function() {
+    hideAllDiv();
+   $("#addPage").show();
+});
+
+
+// click event for button when user clicks it to remove/add movie from the list
+
+
+// replace alert with modal.. confirmation modal tha appears when user select movie that is not available in the db
+
+ // $("#myModal").on("show", function() {    
+ //   $("#myModal a.btn").on("click", function(e) {
+ //      console.log("hellojoy");   
+ //      // dismiss the dialog
+ //      $("#myModal").modal('hide');     
+ //        });
+ //    });
+ // // remove event listener when after dismiss the dialog
+ //    $("#myModal").on("hide", function() {    
+ //        $("#myModal a.btn").off("click");
+ //    });
+ //    // function to remove element from the dom when modal hidden
+ //    $("#myModal").on("hidden", function() {  
+ //        $("#myModal").remove();
+ //    });
+    
+ //    // the actual modal functionality and show the dialog
+ //    $("#myModal").modal({                    
+ //      "backdrop"  : "static",
+ //      "keyboard"  : true,
+ //      "show"      : true                    
+ //    });
 
